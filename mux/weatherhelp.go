6cdc91e18de5b702e21097f5c1693d0d6eaebea3
Sourjaya <sourjaya2013@gmail.com>
2024-03-02 22:49:06 +0530

    added multiplexer for the commands

--- mux/weatherhelp.go
+++ mux/weatherhelp.go
@@ -1,11 +1,38 @@
 package mux
 
 import (
-	"time"
+	"log"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 func (m *Mux) WeatherHelp(ds *discordgo.Session, dm *discordgo.Message, ctx *Context) {
-	ds.ChannelMessageSend(dm.ChannelID, "Ping = "+(time.Since(dm.Timestamp)).String())
+	helpMessage := &discordgo.MessageSend{
+		Embeds: []*discordgo.MessageEmbed{{
+			Type:        discordgo.EmbedTypeRich,
+			Title:       "Help",
+			Description: "Help for Weather Bot",
+			Fields: []*discordgo.MessageEmbedField{
+				{
+					Name:   "Command Syntax",
+					Value:  "!weather <location name>",
+					Inline: true,
+				},
+				{
+					Name:   "Command Example 1",
+					Value:  "!weather kolkata",
+					Inline: true,
+				},
+				{
+					Name:   "Command Example 2",
+					Value:  "!weather Rio De Janeiro",
+					Inline: true,
+				},
+			},
+		},
+		},
+	}
+	if _, err := ds.ChannelMessageSendComplex(dm.ChannelID, helpMessage); err != nil {
+		log.Fatalf("Error while sending message over channel: %v", err)
+	}
 }
